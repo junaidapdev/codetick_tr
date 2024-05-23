077f232fee1538f20b2d11996d233dac83dcd8e1
junaidapdev <junaidap.dev@gmail.com>
2024-05-24 00:37:19 +0530

    first commit

---
+++ src/components/Button.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Button = ({ styles }) => (
+  <a
+    href="https://wa.me/918892786921"
+    className={`inline-block py-4 px-6 font-poppins font-medium text-[18px] text-primary bg-blue-gradient rounded-[10px] ${styles}`}
+  >
+    Whatsapp Now
+  </a>
+);
+
+export default Button;
